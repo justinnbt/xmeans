b8fc85745e1c625170430135fde473c89fd8e032
justinnbt <j.boylan-toomey@outlook.com>
2019-03-03 15:08:58 +0000

    Skeleton

---
+++ xmeans/xmeans.py
@@ -0,0 +1,16 @@
+
+class Cluster:
+    """
+    X-Means clustering class.
+    """
+    def __init__(self):
+        pass
+
+    def bic(self):
+        pass
+
+    def cluster(self):
+        pass
+
+    def fit(self):
+        pass
